e09e0f46a494564e1839e38a8b9bb75001168e87
VDBay <azharsubhan97@gmail.com>
2021-03-23 09:41:45 +0700

    W2-CopyingFileDone

--- W2/CopyingAFile/src/ReadFile.java
+++ W2/CopyingAFile/src/ReadFile.java
@@ -4,19 +4,23 @@ import java.util.Scanner; // Import the Scanner class to read text files
 
 public class ReadFile {
     public static void main(String[] args) {
-        System.out.println("Input file name with extension : ");
-        Scanner scanner = new Scanner(System.in);
-        String filename = scanner.next();
-        try{
-            Scanner scanFile = new Scanner(new File(filename));
-            while (scanFile.hasNextLine()) {
-                String line = scanFile.nextLine();
-                System.out.println(line);
-            }
-        }catch(FileNotFoundException e){
+        String lagi = "y";
+        while (lagi.equalsIgnoreCase("y")){
+            System.out.println("Input file name with extension : ");
+            Scanner scanner = new Scanner(System.in);
+            String filename = scanner.next();
+            try{
+                Scanner scanFile = new Scanner(new File(filename));
+                while (scanFile.hasNextLine()) {
+                    String line = scanFile.nextLine();
+                    System.out.println(line);
+                }
+            }catch(FileNotFoundException e){
                 System.out.println(filename+" Not Found!");
+            }
+            System.out.println("Lagi? (y/n)");
+            lagi=scanner.next();
         }
-
     }
 }
 
