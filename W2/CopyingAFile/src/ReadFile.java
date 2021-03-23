055564c845b99b30312f65fb98324a0181d898d5
VDBay <azharsubhan97@gmail.com>
2021-03-23 09:32:04 +0700

    W2-Another Progress

---
+++ W2/CopyingAFile/src/ReadFile.java
@@ -0,0 +1,22 @@
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+public class ReadFile {
+    public static void main(String[] args) {
+        System.out.println("Input file name with extension : ");
+        Scanner scanner = new Scanner(System.in);
+        String filename = scanner.next();
+        try{
+            Scanner scanFile = new Scanner(new File(filename));
+            while (scanFile.hasNextLine()) {
+                String line = scanFile.nextLine();
+                System.out.println(line);
+            }
+        }catch(FileNotFoundException e){
+                System.out.println(filename+" Not Found!");
+        }
+
+    }
+}
+
